6be4b504dc841486519929d133cbcd9d0a8068fd
pandadiestro <destructo900@gmail.com>
2024-11-20 18:57:13 -0500

    lmao typo

--- src/routes/strings.ts
+++ src/routes/strings.ts
@@ -3,7 +3,7 @@ type StringMap = {
 }
 
 export const strings: StringMap = {
-  "main content": "Hi there! name’s Rodrigo, @pandadiestro anywhere online. I’m a softeng student at the UNMSM university in Perú. I’m currently studying the fields of sound design, machine learning and systems programming",
+  "main content": "Hi there! name’s Rodrigo, @pandadiestro anywhere online. I’m a softeng student at the UNMSM university in Perú. I’m currently studying the fields of sound design, machine learning and systems programming.",
 }
 
 export default {

5cd31b89245f7befb608cea954a533b7a410589c
pandadiestro <destructo900@gmail.com>
2024-12-24 13:06:16 -0500

    added default dark mode

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,34 +1,40 @@
 <script lang="ts">
-    import { strings } from "./strings"
-    import type { PageData } from "./$types"
+    import { strings } from "./strings";
+    import type { PageData } from "./$types";
     import Ghrepo from "$lib/components/ghrepo.svelte";
     import Blinkies from "$lib/components/blinkies.svelte";
     import Socials from "$lib/components/socials.svelte";
 
     type Props = {
-        data: PageData,
+        data: PageData;
     };
 
-    const {
-        data
-    }: Props = $props();
+    const { data }: Props = $props();
 </script>
 
 <svelte:head>
-    <meta name="description" content="pandadiestro's about me page, background, and projects"/>
+    <meta
+        name="description"
+        content="pandadiestro's about me page, background, and projects"
+    />
     <title>about me</title>
 </svelte:head>
 
 <div class="aboutme">{@html strings["main content"]}</div>
 
 <div class="big-separator"></div>
+
 <Socials />
-<div class="big-separator"></div>
-<Blinkies />
-<div class="aboutme">{@html strings["presentation"]}</div>
+
 <div class="big-separator"></div>
 
-{#each data.ghrepos as ghrepo }
+<Blinkies />
+
+<div class="aboutme">{@html strings["presentation"]}</div>
+
+<div class="big-separator"></div>
+
+{#each data.ghrepos as ghrepo}
     <Ghrepo data={ghrepo} />
     <div class="repo-separator"></div>
 {/each}
@@ -47,4 +53,3 @@
         position: relative;
     }
 </style>
-

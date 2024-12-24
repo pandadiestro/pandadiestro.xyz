2d708d75f4b8fb6058c7984a40e69f2b084ea89b
pandadiestro <destructo900@gmail.com>
2024-12-24 15:00:06 -0500

    moved to server loading

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,7 @@
-import type { PageLoad } from "./$types";
+import type { PageServerLoad } from "./$types";
 import type { MinimalRepository } from "$lib/types/GhRepo";
 
-export const load: PageLoad = async({ fetch }) => {
+export const load: PageServerLoad = async({ fetch }) => {
   const res = await fetch('https://api.github.com/users/pandadiestro/repos?sort=created');
   const ghrepos: MinimalRepository[] = await res.json();
   return { ghrepos };
